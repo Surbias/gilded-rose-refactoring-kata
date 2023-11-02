48e84d6ea863a32f64ab5945e5c748e6b96b9fa2
Filipe Almeida <filipe.almeida@moo.com>
2023-11-02 13:54:00 +0000

    Add unit tests to 100% coverage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void unknownItem() {
+        Item[] items = new Item[]{
+            new Item("foo", 0, 1),
+            new Item("bar", 0, 2)
+        };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+
+        assertEquals("bar", app.items[1].name);
+        assertEquals(0, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
     }
 
+    @Test
+    void sulfurasItem() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 1)};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(1, app.items[0].quality);
+        assertEquals(1, app.items[0].quality);
+    }
+
+    @Test
+    void agedBrieItem() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 0)};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(2, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassItem() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void itemToString() {
+        Item item = new Item("baz", 0, 0);
+        assertEquals("baz, 0, 0", item.toString());
+    }
 }
