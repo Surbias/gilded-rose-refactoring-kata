49997a129507ee20782c20d300013a0ffb2a8d18
Filipe Almeida <filipe.almeida@moo.com>
2023-11-04 09:18:42 +0000

    Rename method updateQuality

--- Java/src/main/java/com/gildedrose/item/Item.java
+++ Java/src/main/java/com/gildedrose/item/Item.java
@@ -16,19 +16,19 @@ public class Item {
 
     public void update() {
         this.updateSellIn();
-        this.decreaseQuality();
-
-        if (this.sellIn < 0) {
-            this.decreaseQuality();
-        }
+        this.updateQuality();
     }
 
     protected void updateSellIn() {
         this.sellIn -= 1;
     }
 
-    private void decreaseQuality() {
-        if (this.quality > 0) this.quality -= 1;
+    private void updateQuality() {
+        if (this.quality <= 0) return;
+        this.quality -= 1;
+
+        if (this.quality <= 0) return;
+        if (this.sellIn < 0) this.quality -= 1;
     }
 
 
