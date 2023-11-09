1532ee766c55a195b93da1029bcf9b8d689f2b92
Filipe Almeida <filipe.almeida@moo.com>
2023-11-09 20:59:57 +0000

    Refactor into interface splitting the item specific logic into different Item structs

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,63 +1,102 @@
 package gildedrose
 
-const (
-	agedBrie      = "Aged Brie"
-	backstagePass = "Backstage passes to a TAFKAL80ETC concert"
-	sulfuras      = "Sulfuras, Hand of Ragnaros"
-)
+type ItemUpdater interface {
+	UpdateQuality() *Item
+}
 
 type Item struct {
 	Name            string
 	SellIn, Quality int
 }
 
+func (i *Item) UpdateQuality() *Item {
+	i.SellIn -= 1
+
+	if i.Quality > 0 {
+		i.Quality -= 1
+	}
+
+	if i.SellIn < 0 && i.Quality > 0 {
+		i.Quality -= 1
+	}
+
+	return i
+}
+
+type ItemLegendary struct {
+	Item
+}
+
+func (i *ItemLegendary) UpdateQuality() *Item {
+	return &i.Item
+}
+
+type ItemAgedBrie struct {
+	Item
+}
+
+func (i *ItemAgedBrie) UpdateQuality() *Item {
+	i.SellIn -= 1
+
+	if i.Quality < 50 {
+		i.Quality += 1
+	}
+
+	if i.SellIn < 0 && i.Quality < 50 {
+		i.Quality += 1
+	}
+	return &i.Item
+}
+
+type ItemBackstagePass struct {
+	Item
+}
+
+func (i *ItemBackstagePass) UpdateQuality() *Item {
+	i.SellIn -= 1
+
+	if i.Quality < 50 {
+		i.Quality += 1
+	}
+
+	if i.SellIn < 11 && i.Quality < 50 {
+		i.Quality += 1
+	}
+
+	if i.SellIn < 6 && i.Quality < 50 {
+		i.Quality += 1
+	}
+
+	if i.SellIn < 0 {
+		i.Quality = 0
+	}
+
+	return &i.Item
+}
+
+func createItemUpdater(item *Item) ItemUpdater {
+	const (
+		agedBrie      = "Aged Brie"
+		backstagePass = "Backstage passes to a TAFKAL80ETC concert"
+		sulfuras      = "Sulfuras, Hand of Ragnaros"
+	)
+
+	switch item.Name {
+	case sulfuras:
+		return &ItemLegendary{*item}
+	case agedBrie:
+		return &ItemAgedBrie{*item}
+	case backstagePass:
+		return &ItemBackstagePass{*item}
+	default:
+		return item
+	}
+
+}
+
 func UpdateQuality(items []*Item) {
-	for _, item := range items {
-		if item.Name == sulfuras {
-			continue
-		}
-
-		if item.Name == backstagePass {
-			item.SellIn -= 1
-			if item.Quality < 50 {
-				item.Quality += 1
-			}
-			if item.Quality < 50 {
-				if item.SellIn < 11 {
-					item.Quality += 1
-				}
-				if item.SellIn < 6 && item.Quality < 50 {
-					item.Quality += 1
-				}
-			}
-			if item.SellIn < 0 {
-				item.Quality = 0
-			}
-			continue
-		}
-
-		if item.Name == agedBrie {
-			item.SellIn -= 1
-			if item.Quality < 50 {
-				item.Quality += 1
-			}
-
-			if item.SellIn < 0 && item.Quality < 50 {
-				item.Quality += 1
-			}
-			continue
-		}
-
-		if item.Name != agedBrie && item.Name != backstagePass {
-			if item.Quality > 0 {
-				item.Quality -= 1
-			}
-			item.SellIn -= 1
-			if item.SellIn < 0 && item.Quality > 0 {
-				item.Quality -= 1
-
-			}
-			continue
-		}
+	for i, item := range items {
+		itemUpdater := createItemUpdater(item)
+		items[i] = itemUpdater.UpdateQuality()
 	}
 }
