2770634e030c9b9a2b36e9d164645e2309eaf5b0
Filipe Almeida <filipe.almeida@moo.com>
2023-11-09 17:53:29 +0000

    Extract to variable item names

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,5 +1,11 @@
 package gildedrose
 
+const (
+	agedBrie      = "Aged Brie"
+	backstagePass = "Backstage passes to a TAFKAL80ETC concert"
+	sulfuras      = "Sulfuras, Hand of Ragnaros"
+)
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
@@ -8,16 +14,16 @@ type Item struct {
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+		if items[i].Name != agedBrie && items[i].Name != backstagePass {
 			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+				if items[i].Name != sulfuras {
 					items[i].Quality -= 1
 				}
 			}
 		} else {
 			if items[i].Quality < 50 {
 				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].Name == backstagePass {
 					if items[i].SellIn < 11 {
 						if items[i].Quality < 50 {
 							items[i].Quality += 1
@@ -32,15 +38,15 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+		if items[i].Name != sulfuras {
 			items[i].SellIn -= 1
 		}
 
 		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].Name != agedBrie {
+				if items[i].Name != backstagePass {
 					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+						if items[i].Name != sulfuras {
 							items[i].Quality -= 1
 						}
 					}
