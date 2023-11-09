2eaca1c0c1aa71e53d21b431c9c1f320642bad10
Filipe Almeida <filipe.almeida@moo.com>
2023-11-09 17:50:35 +0000

    Simply increments and decrements

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -11,7 +11,7 @@ func UpdateQuality(items []*Item) {
 		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].Quality > 0 {
 				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
+					items[i].Quality -= 1
 				}
 			}
 		} else {
@@ -20,12 +20,12 @@ func UpdateQuality(items []*Item) {
 				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].SellIn < 11 {
 						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
+							items[i].Quality += 1
 						}
 					}
 					if items[i].SellIn < 6 {
 						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
+							items[i].Quality += 1
 						}
 					}
 				}
@@ -33,7 +33,7 @@ func UpdateQuality(items []*Item) {
 		}
 
 		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
+			items[i].SellIn -= 1
 		}
 
 		if items[i].SellIn < 0 {
@@ -41,15 +41,15 @@ func UpdateQuality(items []*Item) {
 				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].Quality > 0 {
 						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
+							items[i].Quality -= 1
 						}
 					}
 				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
+					items[i].Quality = 0
 				}
 			} else {
 				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
+					items[i].Quality += 1
 				}
 			}
 		}
