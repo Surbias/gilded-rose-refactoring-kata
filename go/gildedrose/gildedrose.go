5ce2d2d53cffd2ec5dfd69eddd50870b125392c7
Filipe Almeida <filipe.almeida@moo.com>
2023-11-09 17:55:07 +0000

    Replace for with for range

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -12,53 +12,51 @@ type Item struct {
 }
 
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].Name != agedBrie && items[i].Name != backstagePass {
-			if items[i].Quality > 0 {
-				if items[i].Name != sulfuras {
-					items[i].Quality -= 1
+	for _, item := range items {
+		if item.Name != agedBrie && item.Name != backstagePass {
+			if item.Quality > 0 {
+				if item.Name != sulfuras {
+					item.Quality -= 1
 				}
 			}
 		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == backstagePass {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality += 1
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
+				if item.Name == backstagePass {
+					if item.SellIn < 11 {
+						if item.Quality < 50 {
+							item.Quality += 1
 						}
 					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality += 1
+					if item.SellIn < 6 {
+						if item.Quality < 50 {
+							item.Quality += 1
 						}
 					}
 				}
 			}
 		}
 
-		if items[i].Name != sulfuras {
-			items[i].SellIn -= 1
+		if item.Name != sulfuras {
+			item.SellIn -= 1
 		}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != agedBrie {
-				if items[i].Name != backstagePass {
-					if items[i].Quality > 0 {
-						if items[i].Name != sulfuras {
-							items[i].Quality -= 1
+		if item.SellIn < 0 {
+			if item.Name != agedBrie {
+				if item.Name != backstagePass {
+					if item.Quality > 0 {
+						if item.Name != sulfuras {
+							item.Quality -= 1
 						}
 					}
 				} else {
-					items[i].Quality = 0
+					item.Quality = 0
 				}
 			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality += 1
+				if item.Quality < 50 {
+					item.Quality += 1
 				}
 			}
 		}
 	}
-
 }
